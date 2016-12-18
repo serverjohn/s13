9e900bb4c503b72f50a9b8c3f4589ff07f1b4b03
John Lundquist <john@jecosolutions.com>
2016-12-17 21:41:55 -0600

    Update requiring Bundler. Upgrade to 4.0.

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
+# Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require(*Rails.groups)
 
 module S13
   class Application < Rails::Application
