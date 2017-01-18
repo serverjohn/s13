be976895bee65c3db1c7fd9386852c82fd420052
John Lundquist <serverjohn@yahoo.com>
2017-01-18 16:48:28 -0600

    Fix CanCan ActiveModel::ForbiddenAttributesError.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  ### work around to get cancan to work. http://stackoverflow.com/questions/17335329/activemodelforbiddenattributeserror-when-creating-new-user
+  before_filter do
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+  ###
+  
   include Authentication
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
