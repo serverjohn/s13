a04b2442f12ae97fd2bfb27e7069498a18f28782
John Lundquist <jlundquist@Johns-MBP.local.tld>
2017-04-26 17:16:47 -0500

    Updated routes.rb to include the disable method for all pertinent controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ S13::Application.routes.draw do
 
   get "settings/index"
 
-
-
   # Root
   root :to => "sessions#new"
 
@@ -11,9 +9,15 @@ S13::Application.routes.draw do
   get "login" => "sessions#new", :as => "login"
   get "logout" => "sessions#destroy", :as => "logout"
   get "checkin" => "checkouts#to_be_checked_in", :as => "checkin"
-  
 
+  # Routes for the disable method in the checkouts, territories, territory_types, users, and worked_with_types controllers.
+  get "ww_disable" => "checkouts#disable", :as => "co_disable"
+  get "ts_disable" => "territories#disable", :as => "ts_disable" 
+  get "tt_disable" => "territory_types#disable", :as => "tt_disable"
+  get "ur_disable" => "users#disable", :as => "ur_disable"
+  get "ww_disable" => "worked_with_types#disable", :as => "ww_disable"
 
+  # Resources
   resources :sessions
   resources :users
   resources :territories
